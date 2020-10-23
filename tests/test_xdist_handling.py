e82d2d2ade8e4c43edd4592b8e37bb6c901ffd93
mrbean-bremen <hansemrbean@googlemail.com>
2020-10-23 21:25:18 +0200

    Add pytest-xdist regression test
    - imported from PR #52, by Andrew Gilbert
    - adapt xdist-versions supported for different pytest versions
    - do not run xdist test for pytest 3.10.1 due to an unrelated problem with that version

---
+++ tests/test_xdist_handling.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import pytest_order
+
+pytest_plugins = ['pytester']
+
+
+@pytest.mark.skipif(pytest.__version__ == "3.10.1",
+                    reason="That version seems to have a parser problem")
+def test_xdist_ordering(tmpdir):
+    testname = str(tmpdir.join("first_test.py"))
+    with open(testname, "w") as fi:
+        fi.write(
+            """
+import pytest
+
+val = 1
+
+@pytest.mark.order("second")
+def test_second_integer():
+    global val
+    assert val == 2
+    val += 1
+
+def test_last_integer():
+    assert val == 3
+
+@pytest.mark.order("first")
+def test_first_integer():
+    global val
+    assert val == 1
+    val += 1
+"""
+        )
+
+    testname = str(tmpdir.join("second_test.py"))
+    with open(testname, "w") as fi:
+        fi.write(
+            """
+import pytest
+
+val = "frog"
+
+@pytest.mark.order("second")
+def test_second_string():
+    global val
+    assert val == "goat"
+    val = "fish"
+
+def test_last_string():
+    assert val == "fish"
+
+@pytest.mark.order("first")
+def test_first_string():
+    global val
+    assert val == "frog"
+    val = "goat"
+"""
+        )
+    # With `loadfile`, the tests should pass
+    args = ["-n3", "--dist=loadfile", str(tmpdir)]
+    ret = pytest.main(args, [pytest_order])
+    assert ret == 0
+
+    # Without `loadfile`, the tests should fail
+    args = ["-n3", str(tmpdir)]
+    ret = pytest.main(args, [pytest_order])
+    assert ret == 1
